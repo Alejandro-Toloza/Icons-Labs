83686cee4e5e7eae6de683ce18cf523d1568710d
Alejandro Toloza <alj70111@gmail.com>
2022-08-02 03:46:27 -0300

    Agregando Entitys

---
+++ icons/src/main/java/com/alkemy/lab/icons/entity/ContinenteEntity.java
@@ -0,0 +1,29 @@
+package com.alkemy.lab.icons.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author alejandro
+ */
+
+@Entity
+@Table(name = "continente")
+@Getter @Setter
+public class ContinenteEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    
+    private String imagen;
+    
+    private String denominacion;
+    
+}
